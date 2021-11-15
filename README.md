7934d77073d71ece684c948449507577fe672297
Sam Sterckval <sam@edgise.com>
2021-11-15 21:51:46 +0100

    alarm base and backup song

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
 # wakerpi
 Raspberry Pi internet radio alarm clock
+
+
+  
+
+Backup song from Bensound : https://www.bensound.com
--- alarm_main.py
+++ alarm_main.py
@@ -1,35 +1,81 @@
 import vlc
 from snoozer_stopper import SnoozerStoper
 import time
+import datetime
 
-VOL_PER_SEC = 2
-VOL_START = 20
+VOL_PER_SEC = 1
+VOL_START = 0
+VOL_MAX = 100
+
+now = datetime.datetime.now()
+
+wakeup_time = datetime.datetime(now.year, now.month, now.day, now.hour, now.minute+1, now.second, now.microsecond)
+
+print(f"setting time for {wakeup_time}")
 
 if __name__ == "__main__":
+    if datetime.date.today().weekday() >= 5:  # then it's a weekend, no wakeup then yet, since this is a test phase
+        exit()
+
+    while wakeup_time - datetime.datetime.now() > datetime.timedelta(0, 30):
+        print(wakeup_time - datetime.datetime.now())
+        time.sleep(1)
+
+    print("Let's start the buffering")
+
     snoozer_stopper = SnoozerStoper(snooze_time=600)
     snoozer_stopper.start()
     vol = VOL_START
-    player = vlc.MediaPlayer("http://icecast.vrtcdn.be/stubru.aac")  # StuBru
+    player = vlc.MediaPlayer()  # StuBru
+    media = vlc.Media("http://icecast.vrtcdn.be/stubru.aac")
+    # player = vlc.MediaPlayer("data/creativeminds.mp3")  # backup song
     player.audio_set_volume(vol)
+    player.set_media(media)
     player.play()
 
-    while not player.get_state() == vlc.State(3):
-        time.sleep(0.5)
-        print(f"State is currently : {player.get_state()}")
-        if player.get_state() == vlc.State(6):
-            print("something went wrong")
+    while True:
+        state = player.get_state()
+        if state == vlc.State.Playing:
+            print(f"Playback on media {player.get_media()} started.")
             break
+        elif state == vlc.State.Opening:
+            time.sleep(0.5)
+            continue
+        elif state == vlc.State.Ended:
+            print(f"Probably an error occured in media {player.get_media()}, going to backup song.")
+            del player
+            player = vlc.MediaPlayer("data/creativeminds.mp3")
+            player.audio_set_volume(vol)
+            player.play()
+            time.sleep(0.5)
+            continue
+        elif state == vlc.State.Buffering:
+            time.sleep(0.5)
+            continue
+        elif state == vlc.State.Error:
+            print(f"An error occured in VLC, loading backup song")
+            del player
+            player = vlc.MediaPlayer("data/creativeminds.mp3")
+            player.audio_set_volume(vol)
+            player.play()
+            time.sleep(0.5)
+            continue
+
+    while wakeup_time - datetime.datetime.now() > datetime.timedelta(0, 10):  # Wait for 10s before to start volume
+        print(f"Sleeping {wakeup_time - datetime.datetime.now()} seconds.")
+        time.sleep((wakeup_time - datetime.datetime.now()).seconds)
+
+    print("Let's go")
 
     interval = time.time()
 
     while not snoozer_stopper.stop_pressed():
         time.sleep(0.2)
-        if time.time() > interval+0.2:
-            if vol < 200:
+        if time.time() > interval:
+            if vol < VOL_MAX:
                 vol += VOL_PER_SEC
             interval = time.time()
             player.audio_set_volume(vol)
-            print(f"vol is now {player.audio_get_volume()}")
 
     player.stop()
     snoozer_stopper.join()
