00fe9c3e43561e134184ae01bdd71a187d81fb37
Sam Sterckval <sam@edgise.com>
2021-11-15 22:19:01 +0100

    trying support for raspberry pi

--- snoozer_stopper.py
+++ snoozer_stopper.py
@@ -1,8 +1,21 @@
 from threading import Thread
 import sys
 from typing import Dict, Callable
-from pynput import keyboard
 import time
+
+if "darwin" in sys.platform:
+    from pynput import keyboard
+elif "linux" in sys.platform:
+    import os
+
+
+def str_to_bool(s) -> bool:
+    if s == 'True':
+        return True
+    elif s == 'False':
+        return False
+    else:
+        raise ValueError("{s} is not a bool")
 
 
 class SnoozerStoper(Thread):
@@ -11,14 +24,35 @@ class SnoozerStoper(Thread):
     STOP_BTN_FACTORY: Dict[str, str] = {"darwin": "<shift>+w",
                                         "linux": "<shift>+w"}
 
+    if "linux" in sys.platform:
+        PATH_TO_TMP = f"/run/user/{os.environ['UID']}/"
+        PATH_TO_SNOOZE = os.path.join(PATH_TO_TMP, "snooze")
+        PATH_TO_STOP = os.path.join(PATH_TO_TMP, "stop")
+
     def __init__(self, snooze_time: int):
         Thread.__init__(self)
         self.snooze_time: int = snooze_time
         self.snoozed: bool = False
         self.stopped: bool = False
-        self.hotkey_dict: Dict[str, Callable] = {self.SNOOZE_BTN_FACTORY[sys.platform]: self._snooze,
-                                                 self.STOP_BTN_FACTORY[sys.platform]: self._stop}
-        self.listener: keyboard.GlobalHotKeys = keyboard.GlobalHotKeys(self.hotkey_dict)
+        if "darwin" in sys.platform:
+            self.hotkey_dict: Dict[str, Callable] = {self.SNOOZE_BTN_FACTORY[sys.platform]: self._snooze,
+                                                     self.STOP_BTN_FACTORY[sys.platform]: self._stop}
+            self.listener: keyboard.GlobalHotKeys = keyboard.GlobalHotKeys(self.hotkey_dict)
+
+        elif "linux" in sys.platform:
+            self.PATH_TO_TMP = f"/run/user/{os.environ['UID']}/"
+
+            if not os.path.exists(self.PATH_TO_TMP):
+                os.makedirs(self.PATH_TO_TMP)
+                print(f"Created run directory @ {self.PATH_TO_TMP}")
+
+            with open(self.PATH_TO_SNOOZE, "w+") as f:
+                f.write("False")
+                f.close()
+
+            with open(self.PATH_TO_STOP, "w+") as f:
+                f.write("False")
+                f.close()
 
     def snooze_pressed(self) -> bool:
         return self.snoozed
@@ -34,16 +68,35 @@ class SnoozerStoper(Thread):
         print("Stop pressed")
         self.stopped = True
 
+    def read_snooze(self):
+        with open(self.PATH_TO_SNOOZE, "r") as f:
+            snz = f.readline()
+            f.close()
+            self.snoozed = str_to_bool(snz)
+
+    def read_stop(self):
+        with open(self.PATH_TO_STOP, "r") as f:
+            snz = f.readline()
+            f.close()
+            self.stopped = str_to_bool(snz)
+
     def run(self) -> None:
-        for key, func in self.hotkey_dict.items():
-            print(f"Press {key} to {str.strip(func.__name__, '_')}")
+        if "darwin" in sys.platform:
+            for key, func in self.hotkey_dict.items():
+                print(f"Press {key} to {str.strip(func.__name__, '_')}")
 
-        self.listener.start()
+            self.listener.start()
 
-        while not self.stop_pressed():
-            time.sleep(0.2)
+            while not self.stop_pressed():
+                time.sleep(0.2)
 
-        self.listener.stop()
-        self.listener.join()
+            self.listener.stop()
+            self.listener.join()
+
+        elif "linux" in sys.platform:
+            while not self.stop_pressed():
+                time.sleep(0.2)
+                self.read_snooze()
+                self.read_stop()
 
         print("Input thread quitting.")
